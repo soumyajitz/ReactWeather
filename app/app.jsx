7a10757a83996221051764bc60ab56d87f8c5b57
soumyajitz <2014soumyajit@gmail.com>
2016-05-23 04:16:23 -0500

    ReactJS App

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,20 @@
 // babeljs.io/repl/
 var React = require('react');
 var ReactDOM = require('react-dom');
-
+var Main = require('Main');
+var Weather = require('Weather');
+var About = require('About');
+var Examples = require('Examples');
+var {Route,Router,IndexRoute,hashHistory} = require('react-router');
+//or we can also write var Route = require('react-router').Route;
 
 ReactDOM.render(
-  <h1>Boilerplate App!!!</h1>,
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+    	<Route path="About" component={About}/>
+    	<Route path="Examples" component={Examples}/>
+    	<IndexRoute component={Weather}/>
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
---
+++ app/components/Nav.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var {Link,IndexLink} = require('react-router');
+var Nav = React.createClass({
+	render:function(){
+		return(
+			<div>
+				<h2>Nav Component</h2>
+				<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Get Weather</IndexLink>
+				<Link to="/About" activeClassName="active" activeStyle={{fontWeight:'bold'}}>About</Link>
+				<Link to="/Examples" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Examples</Link>
+			</div>
+		);
+	}
+});
+
+module.exports = Nav;
